554918f9e9c90e130a2fb08a00097ba86e0cf6a2
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-04-22 23:54:14 -0500

    Initial commit

---
+++ src/main/java/com/pucp/gtics/repaso_lab3_gtics_1/RepasoLab3Gtics1Application.java
@@ -0,0 +1,13 @@
+package com.pucp.gtics.repaso_lab3_gtics_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepasoLab3Gtics1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepasoLab3Gtics1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/pucp/gtics/repaso_lab3_gtics_1/RepasoLab3Gtics1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucp.gtics.repaso_lab3_gtics_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoLab3Gtics1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
