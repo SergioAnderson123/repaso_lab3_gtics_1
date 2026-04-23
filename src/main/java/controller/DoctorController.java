45b45821e0a2f8c6dc6b888b51c3cbb9f086b287
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-04-23 00:53:00 -0500

    Pregunta 4: Doctor list con próximas citas

--- src/main/java/controller/DoctorController.java
+++ src/main/java/controller/DoctorController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import entity.Doctor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import repository.DoctorRepository;
+import repository.PacienteRepository;
+
+@Controller
+@RequestMapping("/doctor")
 public class DoctorController {
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Autowired
+    PacienteRepository pacienteRepository;
+
+    @GetMapping(value = {"", "/"})
+    public String listaDoctores(Model model) {
+        model.addAttribute("listaDoctores", doctorRepository.findAll());
+        return "doctor/list";
+    }
+
+    @GetMapping("/proximasCitas")
+    public String proximasCitas(@RequestParam("id") int doctorId, Model model) {
+        Doctor doctor = doctorRepository.findById(doctorId).orElse(null);
+        model.addAttribute("doctor", doctor);
+        model.addAttribute("pacientes", pacienteRepository.findByDoctorId(doctorId));
+        return "doctor/pacientesList";
+    }
 }
