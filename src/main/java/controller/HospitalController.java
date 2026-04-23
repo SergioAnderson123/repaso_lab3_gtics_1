a4c264f9fd0481c9035023e73840ad69e04053de
SergioAnderson123 <a20223291@pucp.edu.pe>
2026-04-23 00:50:43 -0500

    Pregunta 3: Hospital list con mostrar doctores y pacientes

--- src/main/java/controller/HospitalController.java
+++ src/main/java/controller/HospitalController.java
@@ -1,4 +1,48 @@
 package controller;
 
+import entity.Hospital;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import repository.DoctorRepository;
+import repository.HospitalRepository;
+import repository.PacienteRepository;
+
+@Controller
+@RequestMapping("/hospital")
 public class HospitalController {
+
+    @Autowired
+    HospitalRepository hospitalRepository;
+
+    @Autowired
+    DoctorRepository doctorRepository;
+
+    @Autowired
+    PacienteRepository pacienteRepository;
+
+    @GetMapping(value = {"", "/"})
+    public String listaHospitales(Model model) {
+        model.addAttribute("listaHospitales", hospitalRepository.findAll());
+        return "hospital/list";
+    }
+
+    @GetMapping("/mostrarDoctores")
+    public String mostrarDoctores(@RequestParam("id") int hospitalId, Model model) {
+        Hospital hospital = hospitalRepository.findById(hospitalId).orElse(null);
+        model.addAttribute("hospital", hospital);
+        model.addAttribute("doctores", doctorRepository.findByHospitalId(hospitalId));
+        return "hospital/doctoresList";
+    }
+
+    @GetMapping("/mostrarPacientes")
+    public String mostrarPacientes(@RequestParam("id") int hospitalId, Model model) {
+        Hospital hospital = hospitalRepository.findById(hospitalId).orElse(null);
+        model.addAttribute("hospital", hospital);
+        model.addAttribute("pacientes", pacienteRepository.findByHospitalId(hospitalId));
+        return "hospital/pacientesList";
+    }
 }
